5d7088a305fe7b85843fb9780e0895286785daa4
Drew Partridge <andrpart669@ed.amdsb.ca>
2022-06-08 09:00:02 -0400

    Add project files.

---
+++ dinoJumper/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dinoJumper
+{
+    public partial class Form1 : Form
+    {
+
+        public static int player1Speed = 2;
+        public static int GroundSpikes = 20;
+
+        public static bool wDown = false;
+        public static bool sDown = false;
+        public static bool upArrowDown = false;
+        public static bool downArrowDown = false;
+        public static bool rightArrowDown = false;
+        public static bool leftArrowDown = false;
+
+        public static string gameState = "waiting";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void GameInitialize()
+        {
+            startLabl.Text = "";
+
+
+            gameTimer.Enabled = true;
+            gameState = "running";
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+                case Keys.Left:
+                    leftArrowDown = true;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = true;
+                    break;
+
+
+                case Keys.Space:
+                    if (gameState == "waiting" || gameState == "over" || gameState == "Win" || gameState == "lost")
+
+                    {
+
+                        GameInitialize();
+
+                    }
+
+                    break;
+
+                case Keys.Escape:
+
+                    if (gameState == "waiting" || gameState == "over" || gameState == "Win" || gameState == "lost")
+
+                    {
+
+                        Application.Exit();
+
+                    }
+
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = false;
+                    break;
+                case Keys.Left:
+                    leftArrowDown = false;
+                    break;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
